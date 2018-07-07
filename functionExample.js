071c4ff733029825b951e5e50de02fa011975c7a
AAPL85 <abel.perez@aisbusiness.mx>
2018-07-07 11:44:01 -0600

    Instalando JS Script

--- functionExample.js
+++ functionExample.js
@@ -7,4 +7,11 @@ function suma(num1, num2){
  console.log("El valor de la suma es de: ");
  console.log(sum);
 }
+
+function div(num1, num2){
+ div = num1 / num2;
+ console.log("El valor de la divicion es de: ");
+ console.log(div);
+}
  suma(x,y);
+ div(x,y);
